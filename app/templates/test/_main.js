ea977308f674adace6be145841784a6e3d41b162
Hector G. Parra <hector@hectorparra.com>
2013-12-21 15:49:21 -0800

    fix(template-test): improved testing scaffolding
    * "expected" files
    * test: error on stream
    * test: on stream (example)
    
    Closes #6

--- app/templates/test/_main.js
+++ app/templates/test/_main.js
@@ -1,8 +1,9 @@
 /*global describe, it*/
 "use strict";
 
-var fs = require("fs");
-require("should");
+var fs = require("fs"),
+	es = require("event-stream"),
+	should = require("should");
 require("mocha");
 
 var gutil = require("gulp-util"),
@@ -10,24 +11,99 @@ var gutil = require("gulp-util"),
 
 describe("<%= _.slugify(appname) %>", function () {
 
-	var file = new gutil.File({
-		path: "test/fixtures/hello.txt",
+	var expectedFile = new gutil.File({
+		path: "test/expected/hello.txt",
 		cwd: "test/",
-		base: "test/fixtures",
-		contents: fs.readFileSync("test/fixtures/hello.txt")
+		base: "test/expected",
+		contents: fs.readFileSync("test/expected/hello.txt")
 	});
 
-	it("should work", function (done) {
+	it("should produce expected file via buffer", function (done) {
 
-		var stream = <%= pluginName %>("Bye");
+		var srcFile = new gutil.File({
+			path: "test/fixtures/hello.txt",
+			cwd: "test/",
+			base: "test/fixtures",
+			contents: fs.readFileSync("test/fixtures/hello.txt")
+		});
 
-		stream.on("error", done);
-		stream.on("data", function (file) {
-			String(file.contents).should.equal("Hello\nBye");
+		var stream = <%= pluginName %>("World");
+
+		stream.on("error", function(err) {
+			should.exist(err);
+			done(err);
+		});
+
+		stream.on("data", function (newFile) {
+
+			should.exist(newFile);
+			should.exist(newFile.contents);
+
+			String(newFile.contents).should.equal(String(expectedFile.contents));
 			done();
 		});
 
-		stream.write(file);
+		stream.write(srcFile);
 		stream.end();
 	});
+
+	it("should error on stream", function (done) {
+
+		var srcFile = new gutil.File({
+			path: "test/fixtures/hello.txt",
+			cwd: "test/",
+			base: "test/fixtures",
+			contents: fs.createReadStream("test/fixtures/hello.txt")
+		});
+
+		var stream = <%= pluginName %>("World");
+
+		stream.on("error", function(err) {
+			should.exist(err);
+			done();
+		});
+
+		stream.on("data", function (newFile) {
+			newFile.contents.pipe(es.wait(function(err, data) {
+				done(err);
+			}));
+		});
+
+		stream.write(srcFile);
+		stream.end();
+	});
+
+	/*
+	it("should produce expected file via stream", function (done) {
+
+		var srcFile = new gutil.File({
+			path: "test/fixtures/hello.txt",
+			cwd: "test/",
+			base: "test/fixtures",
+			contents: fs.createReadStream("test/fixtures/hello.txt")
+		});
+
+		var stream = <%= pluginName %>("World");
+
+		stream.on("error", function(err) {
+			should.exist(err);
+			done();
+		});
+
+		stream.on("data", function (newFile) {
+
+			should.exist(newFile);
+			should.exist(newFile.contents);
+
+			newFile.contents.pipe(es.wait(function(err, data) {
+				should.not.exist(err);
+				data.should.equal(String(expectedFile.contents));
+				done();
+			}));
+		});
+
+		stream.write(srcFile);
+		stream.end();
+	});
+	*/
 });
