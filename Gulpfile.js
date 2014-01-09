adf585b7b2c1fd89bfdf7bf5c010d7c101d1d543
Mikael Brevik <mikaelbre@gmail.com>
2014-01-09 18:12:41 +0100

    Fixes gulpfile to run jshint on files (with updated dependancies)

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,46 +1,14 @@
 "use strict";
 
 var gulp = require("gulp"),
-	fs = require("fs"),
+	join = require("path").join,
 	jshint = require("gulp-jshint");
 
 // JSHint
 // https://github.com/wearefractal/gulp-jshint
 gulp.task("jshint", function () {
-
-	//
-	// FIXME
-	//
-	// gulp.jshint not reading .jshintrc
-	// side-effect of JSHint itself not read configuration when using stdin
-	//
-	// https://github.com/wearefractal/gulp-jshint/issues/4
-	//
-
-	var options = JSON.parse(fs.readFileSync(".jshintrc", "utf8"));
-
-	//
-	// FIXME
-	//
-	// Can't use following due to Error: EMFILE, too many open files:
-	//
-	//	gulp.src("./**/*.js")
-	//		.pipe(ignore({
-	//			pattern: [
-	//				"./node_modules/**",
-	//				"./test/temp/**"]}))
-	//
-	// Must wait for core impl of .src() ignores
-	//
-	// https://github.com/wearefractal/gulp/issues/35
-	//
-
-	gulp.src("./*.js")
-		.pipe(jshint(options))
-		.pipe(jshint.reporter("default"));
-
-	gulp.src("./app/*.js")
-		.pipe(jshint(options))
+	gulp.src(["./**/*.js", "!node_modules/**", "!test/temp/**"])
+		.pipe(jshint(join(__dirname, ".jshintrc")))
 		.pipe(jshint.reporter("default"));
 });
 
