7f61477e915c9ecfc86c05a638f0fd546e1ee440
Daniel Furze <daniel@furzeface.com>
2014-11-09 18:29:53 +0000

    fix(template-index): Adds config.save to produce .yo-rc.json - resolves #21

--- app/index.js
+++ app/index.js
@@ -47,6 +47,7 @@ var GulpPluginGenerator = module.exports = function GulpPluginGenerator(args, op
 
 	this.on("end", function () {
 		this.installDependencies({ skipInstall: options["skip-install"] });
+		this.config.save();
 	});
 
 	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, "../package.json")));
